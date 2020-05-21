0f31c0a53f12568be5294b5a352362be16c6644e
aSilas <silastj@hotmail.com>
2020-05-21 00:13:21 -0300

    add os click com js

--- js/slide.js
+++ js/slide.js
@@ -6,18 +6,38 @@ export default class Slide {
     }
 
 
-onselectstart(event){
+onStart(event){
     event.preventDefault();
-    console.log(event);
+    console.log('mousedown');
+    this.wrapper.addEventListener('mousemove', this.onMove);
+}
+
+onMove(event){
+    console.log('moveu');
+}
+
+onEnd(event){
+    console.log('acabou');
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+
 }
 
 addSlideEvents(){
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+
 }
 
 
+bindEvents(){
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+}
+
 
 init(){
+    this.bindEvents();
     this.addSlideEvents();
     return this;
 }
