f1a9d61766658d5bedc2c534baa7636a86add2fb
aSilas <silastj@hotmail.com>
2020-05-21 22:30:56 -0300

    Calculo de distancias quando o evento ocorre no passar das fotos

--- js/slide.js
+++ js/slide.js
@@ -3,44 +3,55 @@ export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper =  document.querySelector(wrapper);
+        this.dist = { finalPosition: 0, startX: 0,movement:0 }
     }
 
 
-onStart(event){
-    event.preventDefault();
-    console.log('mousedown');
-    this.wrapper.addEventListener('mousemove', this.onMove);
-}
+    moveSlide(distX){
+        this.dist.movePositon = distX;
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+    }
 
-onMove(event){
-    console.log('moveu');
-}
+    onStart(event){
+        event.preventDefault();
+        this.dist.startX = event.clientX;
+        this.wrapper.addEventListener('mousemove', this.onMove);
+    }
 
-onEnd(event){
-    console.log('acabou');
-    this.wrapper.removeEventListener('mousemove', this.onMove);
+    updatePosition(clienteX){
+        this.dist.movement = (this.dist.startX - clienteX) * 1.6;
+        return this.dist.finalPosition - this.dist.movement;
+    }
 
-}
+    onMove(event){
+        const finalPosition = this.updatePosition(event.clientX);
+        this.moveSlide(finalPosition);
+    }
 
-addSlideEvents(){
-    this.wrapper.addEventListener('mousedown', this.onStart);
-    this.wrapper.addEventListener('mouseup', this.onEnd);
+    onEnd(event){
+        this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.dist.finalPosition = this.dist.movePositon;
 
-}
+    }
 
-
-bindEvents(){
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-}
+    addSlideEvents(){
+        this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
+    }
 
 
-init(){
-    this.bindEvents();
-    this.addSlideEvents();
-    return this;
-}
+    bindEvents(){
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+    }
+
+
+    init(){
+        this.bindEvents();
+        this.addSlideEvents();
+        return this;
+    }
 
 
 
