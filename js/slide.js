edfc7db9ffd8d55a7a1efda02b7cea0e2b1b5847
aSilas <silastj@hotmail.com>
2020-05-21 22:57:45 -0300

    Events de touch mobile ad

--- js/slide.js
+++ js/slide.js
@@ -13,9 +13,17 @@ export default class Slide {
     }
 
     onStart(event){
-        event.preventDefault();
-        this.dist.startX = event.clientX;
-        this.wrapper.addEventListener('mousemove', this.onMove);
+        let movetype;
+        if(event.type === 'mousedown'){
+            event.preventDefault();
+            this.dist.startX = event.clientX;
+            movetype = 'mousemove';
+        }else{
+            this.dist.startX = event.changedTouches[0].clientX;
+            movetype = 'touchmove';
+        }
+        this.wrapper.addEventListener(movetype, this.onMove);
+
     }
 
     updatePosition(clienteX){
@@ -24,19 +32,22 @@ export default class Slide {
     }
 
     onMove(event){
-        const finalPosition = this.updatePosition(event.clientX);
+        const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+        const finalPosition = this.updatePosition(pointerPosition);
         this.moveSlide(finalPosition);
     }
 
     onEnd(event){
-        this.wrapper.removeEventListener('mousemove', this.onMove);
+        const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+        this.wrapper.removeEventListener(movetype, this.onMove);
         this.dist.finalPosition = this.dist.movePositon;
-
     }
 
     addSlideEvents(){
         this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('touchstar', this.onStart);
         this.wrapper.addEventListener('mouseup', this.onEnd);
+        this.wrapper.addEventListener('touchend', this.onEnd);
     }
 
 
